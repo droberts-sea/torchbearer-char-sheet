2013605532b425d337d1ecf29b3a74a0e511d34e
Dan Roberts <dan@adadevelopersacademy.org>
2018-04-22 11:43:28 -0700

    Whitespace changes

--- src/components/roll/RollSummary.js
+++ src/components/roll/RollSummary.js
@@ -3,14 +3,13 @@ import React from 'react';
 const RollSummary = function(props) {
   return (
     <footer id="roll-summary">
-    <label className="label" htmlFor="dice">Dice</label>
-    <label className="label" htmlFor="ob">
-    {props.type}
-    </label>
-    <label className="label" htmlFor="odds">Odds</label>
-    <span className="number" name="dice">{props.dice}</span>
-    <span className="number" name="ob">{props.ob}</span>
-    <span className="number" name="odds">{props.odds}%</span>
+      <label className="label" htmlFor="dice">Dice</label>
+      <label className="label" htmlFor="ob">{props.type}</label>
+      <label className="label" htmlFor="odds">Odds</label>
+
+      <span className="number" name="dice">{props.dice}</span>
+      <span className="number" name="ob">{props.ob}</span>
+      <span className="number" name="odds">{props.odds}%</span>
     </footer>
   );
 };
