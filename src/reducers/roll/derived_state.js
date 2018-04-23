96cfc014111ab2eec5524cc613902f1aa98d9ad2
Dan Roberts <dan@adadevelopersacademy.org>
2018-04-22 19:48:36 -0700

    Add tests for dice math

--- src/reducers/roll/derived_state.js
+++ src/reducers/roll/derived_state.js
@@ -1,3 +1,6 @@
+import math from 'mathjs';
+import _ from 'underscore';
+
 const InitialState = {
   summary: {
     type: 'obstacle',
@@ -77,6 +80,38 @@ const skillDice = function(state, character, summary, details) {
   }
 }
 
+export const expectedMargin = function(summary) {
+  const expected_success = (summary.dice / 2.0) + summary.successes;
+  let expected_bar;
+  if (summary.type == 'versus') {
+    expected_bar = summary.ob / 2;
+  } else {
+    expected_bar = summary.ob;
+  }
+  return expected_success - expected_bar;
+}
+
+export const oddsOfSuccess = function(summary) {
+  // TODO: what about versus tests?
+  if (summary.ob - summary.successes <= 0) {
+    return 1;
+  }
+
+  // Sum from i=ob to number of dice d of
+  //   (d choose i) * .5^i * .5^(d-i)
+  const dice = summary.dice;
+  const base = summary.ob - summary.successes;
+  return math.sum(_.times(1 + dice - base, (j) => {
+    const i = j + base;
+    return math.combinations(dice, i) * (.5 ** i) * (.5 ** (dice-i));
+  }));
+}
+
+const diceMath = function(state, character, summary, details) {
+  summary.expected_margin = expectedMargin(summary);
+  summary.odds = oddsOfSuccess(summary);
+}
+
 const calculateDerivedRollState = function(state, character) {
   if (!character) {
     return InitialState;
@@ -98,7 +133,7 @@ const calculateDerivedRollState = function(state, character) {
 
   // TODO: dice from everything else
 
-  // TODO: dice math - calculate a success chance
+  diceMath(state, character, summary, details);
 
   console.log(details);
 
