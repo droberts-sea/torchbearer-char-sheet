63473c941ffd3a2d373e1703ed4eb03b7fe76d37
Dan Roberts <dan@adadevelopersacademy.org>
2020-01-20 08:53:26 -0700

    Some work I was doing before I guess?

--- src/reducers/roll/test/derived_state.test.js
+++ src/reducers/roll/test/derived_state.test.js
@@ -1,7 +1,9 @@
-import { skillDice } from '../derived_state';
+import _ from 'underscore';
+
+import { skillDice, calculateDerivedRollState } from '../derived_state';
 import character from '../../../mock/character';
 
-describe('skillDice', () => {
+describe('Derived State', () => {
   let rollState;
   let rollSummary;
   beforeEach(() => {
@@ -22,54 +24,103 @@ describe('skillDice', () => {
     }
   });
 
-  const checkRatingAdded = (skillName, expectedDice, natureInstead=false) => {
-    rollState.dice.info.skill = skillName;
-    rollState.dice.modifiers.natureInstead = natureInstead;
+  describe('skillDice', () => {
 
-    const details = [];
-    skillDice(rollState, character, rollSummary, details);
 
-    expect(rollSummary.dice).toBe(expectedDice);
-    expect(details.length).toBe(1);
-  }
+    const checkRatingAdded = (skillName, expectedDice, natureInstead=false) => {
+      rollState.dice.info.skill = skillName;
+      rollState.dice.modifiers.natureInstead = natureInstead;
 
-  test('Adds full dice for a trained skill', () => {
-    const skillName = 'ORATOR';
-    const skill = character.skills[skillName];
+      const details = [];
+      skillDice(rollState, character, rollSummary, details);
 
-    // Assumption: ORATOR is an open skill
-    expect(skill.open).toBe(true);
+      expect(rollSummary.dice).toBe(expectedDice);
+      expect(details.length).toBe(1);
+    }
 
-    checkRatingAdded(skillName, skill.rating);
-    expect(rollSummary.isBeginnersLuck).toBe(false);
+    test('Adds full dice for a trained skill', () => {
+      const skillName = 'ORATOR';
+      const skill = character.skills[skillName];
+
+      // Assumption: ORATOR is an open skill
+      expect(skill.open).toBe(true);
+
+      checkRatingAdded(skillName, skill.rating);
+      expect(rollSummary.isBeginnersLuck).toBe(false);
+    });
+
+    test('Adds full dice for an ability', () => {
+      const abilityName = 'WILL';
+      const ability = character.abilities[abilityName];
+
+      checkRatingAdded(abilityName, ability.rating);
+      expect(rollSummary.isBeginnersLuck).toBe(false);
+    });
+
+    test('Uses nature if rolling with nature', () => {
+      const nature = character.abilities['NATURE'];
+
+      checkRatingAdded('ALCHEMIST', nature.rating, true);
+      expect(rollSummary.isBeginnersLuck).toBe(false);
+    });
+
+    test('Uses BL ability and sets isBeginnersLuck for an untrained skill', () => {
+      const skillName = 'ALCHEMIST';
+      const skill = character.skills[skillName];
+
+      // Assumption: ALCHEMIST is not an open skill
+      expect(skill.open).toBe(false);
+
+      const blAbility = character.abilities[skill.beginnersLuck];
+      const expectedDice = blAbility.rating;
+
+      checkRatingAdded(skillName, expectedDice);
+      expect(rollSummary.isBeginnersLuck).toBe(true);
+    });
   });
 
-  test('Adds full dice for an ability', () => {
-    const abilityName = 'WILL';
-    const ability = character.abilities[abilityName];
+  describe("Beginner's Luck", () => {
+    let blSkillName = 'ALCHEMIST';
+    let trainedSkillName = 'ORATOR';
+    beforeAll(() => {
+      expect(character.skills[blSkillName].open).toBe(false);
+      expect(character.skills[trainedSkillName].open).toBe(true);
+    });
 
-    checkRatingAdded(abilityName, ability.rating);
-    expect(rollSummary.isBeginnersLuck).toBe(false);
-  });
+    describe("Affected by BL", () => {
+      test.skip("wises", () => {
 
-  test('Uses nature if rolling with nature', () => {
-    const nature = character.abilities['NATURE'];
+      });
 
-    checkRatingAdded('ALCHEMIST', nature.rating, true);
-    expect(rollSummary.isBeginnersLuck).toBe(false);
-  });
+      test("help", () => {
+        const skill = character.skills[blSkillName];
+        const helpDice = 4;
 
-  test('Uses BL ability and sets isBeginnersLuck for an untrained skill', () => {
-    const skillName = 'ALCHEMIST';
-    const skill = character.skills[skillName];
+        rollState.dice.info.skill = blSkillName;
+        rollState.dice.modifiers.natureInstead = false;
+        rollState.dice.modifiers.help = 0;
 
-    // Assumption: ALCHEMIST is not an open skill
-    expect(skill.open).toBe(false);
+        const beforeState = calculateDerivedRollState(rollState, character);
 
-    const blAbility = character.abilities[skill.beginnersLuck];
-    const expectedDice = blAbility.rating;
+        rollState.dice.modifiers.help = helpDice;
 
-    checkRatingAdded(skillName, expectedDice);
-    expect(rollSummary.isBeginnersLuck).toBe(true);
+        const afterState = calculateDerivedRollState(rollState, character);
+
+        console.log(beforeState);
+        console.log(afterState);
+        const expectedDice = beforeState.summary.dice + helpDice / 2;
+        expect(afterState.summary.dice).toBe(expectedDice);
+
+        expect(
+          _.any(details, (detail) => {
+            detail.source.includes('Help');
+          })
+        ).toBe(true);
+      })
+    });
+
+    describe("Not affected by BL", () => {
+
+    });
   });
 });
