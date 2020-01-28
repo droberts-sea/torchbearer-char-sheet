286527e6cbb4e5217c052e6855105c32378ee3cb
Dan Roberts <dan@adadevelopersacademy.org>
2020-01-28 12:37:18 -0700

    Reorder the dice summary component to make more sense

--- src/components/roll/RollSummary.js
+++ src/components/roll/RollSummary.js
@@ -3,16 +3,16 @@ import React from 'react';
 const RollSummary = function(props) {
   return (
     <footer id="roll-summary">
-      <label className="label" htmlFor="dice">Dice</label>
       <label className="label" htmlFor="ob">{props.type}</label>
+      <label className="label" htmlFor="dice">Dice</label>
       <label className="label" htmlFor="successes">Successes</label>
       <label className="label" htmlFor="odds">Odds</label>
       <label className="label" htmlFor="expected_margin">Margin</label>
 
-      <span className="number" name="dice">{props.dice}</span>
       <span className="number" name="ob">{props.ob}</span>
+      <span className="number" name="dice">{props.dice}</span>
       <span className="number" name="successes">
-        {props.successes > 0 ? '+' : '' }
+        {props.successes >= 0 ? '+' : '-' }
         {props.successes}
       </span>
       <span className="number" name="odds">{(props.odds * 100).toFixed()}%</span>
