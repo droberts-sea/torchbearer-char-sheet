a7c85718e2dcc725abdfce5da7609ddf8953611d
Dan Roberts <dan@adadevelopersacademy.org>
2020-02-09 14:32:37 -0800

    Add new file (derp)

---
+++ src/rules/initial_character.js
@@ -0,0 +1,148 @@
+import { ConditionRules } from './conditions';
+import { SkillRules } from './skills';
+
+// See _Abilities_, page 26
+export const InitialAbilities = Object.freeze({
+  NATURE: {
+    name: 'Nature',
+    rating: 3,
+    untaxed: 5,
+    min: 0,
+    max: 7,
+    advancement: {
+      pass: 0,
+      fail: 0
+    },
+    open: true, // All abilities are always open
+    descriptors: ['Sneaking', 'Riddling', 'Merrymaking']
+  },
+  WILL: {
+    name: 'Will',
+    rating: 5,
+    min: 1,
+    max: 6,
+    advancement: {
+      pass: 0,
+      fail: 0
+    },
+    open: true
+  },
+  HEALTH: {
+    name: 'Health',
+    rating: 3,
+    min: 1,
+    max: 6,
+    advancement: {
+      pass: 0,
+      fail: 0
+    },
+    open: true
+  },
+  RESOURCES: {
+    name: 'Resources',
+    rating: 3,
+    min: 0,
+    max: 10,
+    advancement: {
+      pass: 0,
+      fail: 0
+    },
+    open: true
+  },
+  CIRCLES: {
+    name: 'Circles',
+    rating: 4,
+    min: 1,
+    max: 10,
+    advancement: {
+      pass: 0,
+      fail: 0
+    },
+    open: true
+  },
+  MIGHT: {
+    name: 'Might',
+    rating: 3,
+    open: true // do you ever even roll might?
+  }
+});
+
+const conditions = {};
+Object.keys(ConditionRules).forEach((name) => {
+  conditions[name] = ConditionRules[name].default_state;
+});
+export const InitialConditions = Object.freeze(conditions);
+
+export const InitialPoints = Object.freeze({
+  fate: {
+    available: 3,
+    spent: 0
+  },
+  persona: {
+    available: 3,
+    spent: 0
+  },
+  checks: {
+    available: 0,
+    spent: 0
+  }
+});
+
+export const InitialSkills = Object.freeze(
+  Object.keys(SkillRules).map((name) => {
+    const display_name = name.split('_').map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }).join(' ');
+    return {
+      beginnersLuck: SkillRules[name].beginnersLuck,
+      name: display_name,
+      rating: 0,
+      min: 0,
+      max: 6, // Skills are rated from 1 to 6 (pg 13)
+      advancement: {
+        pass: 0,
+        fail: 0
+      },
+      open: false // Skill rating can be 0 but still open
+    };
+  })
+);
+
+export const InitialTraits = Object.freeze([
+  {
+    name: "Firey",
+    level: 1,
+    uses: 0
+  },
+  {
+    name: "Jaded",
+    level: 2,
+    uses: 2
+  },
+  {
+    name: "Curious",
+    level: 3,
+    uses: 0
+  }
+]);
+
+export const InitialWises = Object.freeze([
+  {
+    name: "Deepest Heart of Darkness",
+    advancement: {
+      pass: true,
+      fail: true,
+      fate: true,
+      persona: false
+    }
+  },
+  {
+    name: "Home",
+    advancement: {
+      pass: false,
+      fail: false,
+      fate: false,
+      persona: true
+    }
+  }
+]);
