1467a2f56fac1e1937c20625c01d7e2f3fc8ea96
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-22 23:05:46 +0000

    Deploy droberts-ada/torchbearer-char-sheet to github.com/droberts-ada/torchbearer-char-sheet.git:gh-pages

---
+++ precache-manifest.343333bd3b273e231b17afe9ecb5065d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceced9e676c6dc3f9fe684b5adc1613c",
+    "url": "/torchbearer-char-sheet/index.html"
+  },
+  {
+    "revision": "684a6e2a6063fd42a3bd",
+    "url": "/torchbearer-char-sheet/static/css/main.ce25a76e.chunk.css"
+  },
+  {
+    "revision": "dda11ede9dc3a13a07c1",
+    "url": "/torchbearer-char-sheet/static/js/2.5776a2f0.chunk.js"
+  },
+  {
+    "revision": "a7eed2f310b82a6d5c4409c9acf7cf5a",
+    "url": "/torchbearer-char-sheet/static/js/2.5776a2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684a6e2a6063fd42a3bd",
+    "url": "/torchbearer-char-sheet/static/js/main.79cff7ba.chunk.js"
+  },
+  {
+    "revision": "93b323bb8ac0c5a7c544",
+    "url": "/torchbearer-char-sheet/static/js/runtime-main.db05ba9a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/torchbearer-char-sheet/precache-manifest.343333bd3b273e231b17afe9ecb5065d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/torchbearer-char-sheet/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
